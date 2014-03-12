78f5dd29948c2af846e41e5ab29b2000249b90fb
doyler <dev@doyler.net>
2014-03-12 14:25:16 -0400

    Initial check-in, basic form
    Initial check-in for files with the basic form layout and error messages

---
+++ FusionCalculator/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace FusionCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.armor1element1 = new System.Windows.Forms.ComboBox();
+            this.armor1element2 = new System.Windows.Forms.ComboBox();
+            this.armor1stars = new System.Windows.Forms.ComboBox();
+            this.armor2element1 = new System.Windows.Forms.ComboBox();
+            this.armor2element2 = new System.Windows.Forms.ComboBox();
+            this.armor2stars = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.calculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // armor1element1
+            // 
+            this.armor1element1.FormattingEnabled = true;
+            this.armor1element1.Items.AddRange(new object[] {
+            "Fire",
+            "Spirit",
+            "Earth",
+            "Air",
+            "Water",
+            "None"});
+            this.armor1element1.Location = new System.Drawing.Point(12, 53);
+            this.armor1element1.Name = "armor1element1";
+            this.armor1element1.Size = new System.Drawing.Size(121, 21);
+            this.armor1element1.TabIndex = 0;
+            // 
+            // armor1element2
+            // 
+            this.armor1element2.FormattingEnabled = true;
+            this.armor1element2.Items.AddRange(new object[] {
+            "Fire",
+            "Spirit",
+            "Earth",
+            "Air",
+            "Water",
+            "None"});
+            this.armor1element2.Location = new System.Drawing.Point(139, 53);
+            this.armor1element2.Name = "armor1element2";
+            this.armor1element2.Size = new System.Drawing.Size(121, 21);
+            this.armor1element2.TabIndex = 1;
+            // 
+            // armor1stars
+            // 
+            this.armor1stars.FormattingEnabled = true;
+            this.armor1stars.Items.AddRange(new object[] {
+            "One",
+            "Two",
+            "Three",
+            "Four",
+            "Five"});
+            this.armor1stars.Location = new System.Drawing.Point(266, 53);
+            this.armor1stars.Name = "armor1stars";
+            this.armor1stars.Size = new System.Drawing.Size(121, 21);
+            this.armor1stars.TabIndex = 2;
+            // 
+            // armor2element1
+            // 
+            this.armor2element1.FormattingEnabled = true;
+            this.armor2element1.Items.AddRange(new object[] {
+            "Fire",
+            "Spirit",
+            "Earth",
+            "Air",
+            "Water",
+            "None"});
+            this.armor2element1.Location = new System.Drawing.Point(12, 106);
+            this.armor2element1.Name = "armor2element1";
+            this.armor2element1.Size = new System.Drawing.Size(121, 21);
+            this.armor2element1.TabIndex = 3;
+            // 
+            // armor2element2
+            // 
+            this.armor2element2.FormattingEnabled = true;
+            this.armor2element2.Items.AddRange(new object[] {
+            "Fire",
+            "Spirit",
+            "Earth",
+            "Air",
+            "Water",
+            "None"});
+            this.armor2element2.Location = new System.Drawing.Point(139, 106);
+            this.armor2element2.Name = "armor2element2";
+            this.armor2element2.Size = new System.Drawing.Size(121, 21);
+            this.armor2element2.TabIndex = 4;
+            // 
+            // armor2stars
+            // 
+            this.armor2stars.FormattingEnabled = true;
+            this.armor2stars.Items.AddRange(new object[] {
+            "One",
+            "Two",
+            "Three",
+            "Four",
+            "Five"});
+            this.armor2stars.Location = new System.Drawing.Point(266, 106);
+            this.armor2stars.Name = "armor2stars";
+            this.armor2stars.Size = new System.Drawing.Size(121, 21);
+            this.armor2stars.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Armor 1 Element 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(136, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Armor 1 Element 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(263, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Armor 1 Stars";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Armor 2 Element 1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(136, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Armor 2 Element 2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(263, 90);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Armor 2 Stars";
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(154, 160);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(75, 23);
+            this.calculate.TabIndex = 12;
+            this.calculate.Text = "Calculate";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 428);
+            this.Controls.Add(this.calculate);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.armor2stars);
+            this.Controls.Add(this.armor2element2);
+            this.Controls.Add(this.armor2element1);
+            this.Controls.Add(this.armor1stars);
+            this.Controls.Add(this.armor1element2);
+            this.Controls.Add(this.armor1element1);
+            this.Name = "Form1";
+            this.Text = "Fusion Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox armor1element1;
+        private System.Windows.Forms.ComboBox armor1element2;
+        private System.Windows.Forms.ComboBox armor1stars;
+        private System.Windows.Forms.ComboBox armor2element1;
+        private System.Windows.Forms.ComboBox armor2element2;
+        private System.Windows.Forms.ComboBox armor2stars;
+
+        private object[] elements;
+        private object[] stars;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button calculate;
+    }
+}
+
---
+++ FusionCalculator/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FusionCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.Print("Calculate");
+
+            if (armor1element1.Text == "")
+            {
+                showError("You must select an element 1 for armor 1");
+            }
+            else if (armor1element2.Text == "")
+            {
+                showError("You must select an element 2 for armor 1 (select none for mono-element)");
+            }
+            else if (armor2element1.Text == "")
+            {
+                showError("You must select an element 1 for armor 2");
+            }
+            else if (armor2element2.Text == "")
+            {
+                showError("You must select an element 2 for armor 2 (select none for mono-element)");
+            }
+            else if (armor1element1.Text == armor1element2.Text)
+            {
+                showError("Armor 1 cannot have 2 of the same element.");
+            }
+            else if (armor2element1.Text == armor2element2.Text)
+            {
+                showError("Armor 2 cannot have 2 of the same element.");
+            }
+        }
+
+        private void showError(string theError)
+        {
+            MessageBox.Show(theError, "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+        }
+    }
+}
