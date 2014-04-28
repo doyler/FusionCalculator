280c9e0bc9cf001c401d766c21f62bf894f4820e
doyler <dev@doyler.net>
2014-04-27 22:27:36 -0400

    Better pathing for csv files, adding csv files

--- FusionCalculator/Form1.cs
+++ FusionCalculator/Form1.cs
@@ -222,13 +222,13 @@ namespace FusionCalculator
         private DataTable CsvToDataTable(string strFileName)
         {
             DataTable dataTable = new DataTable("DataTable Name");
+            string fullPath = Path.GetDirectoryName("..\\..\\" + strFileName);
 
-            using (OleDbConnection conn = new OleDbConnection("Provider=Microsoft.Jet.OleDb.4.0; Data Source = " + Directory.GetCurrentDirectory() + "; Extended Properties = \"Text;HDR=YES;FMT=Delimited\""))
+            using (OleDbConnection conn = new OleDbConnection("Provider=Microsoft.Jet.OleDb.4.0; Data Source = " + fullPath + "; Extended Properties = \"Text;HDR=YES;FMT=Delimited\""))
             {
                 conn.Open();
                 string strQuery = "SELECT * FROM [" + strFileName + "]";
-                OleDbDataAdapter adapter =
-                    new System.Data.OleDb.OleDbDataAdapter(strQuery, conn);
+                OleDbDataAdapter adapter = new System.Data.OleDb.OleDbDataAdapter(strQuery, conn);
                 adapter.Fill(dataTable);
             }
             return dataTable;
